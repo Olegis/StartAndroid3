b3b70e9af10d5c30d737b9db39bde47edc72bc43
olegis85 <olegis85@mail.ru>
2020-08-03 19:29:07 +0600

    Tab - вкладки. TabActivity. Activity, как содержимое вкладки (устаревшее)

---
+++ p0771_tabintent/src/main/java/com/olegis/p0771_tabintent/TwoActivity.java
@@ -0,0 +1,14 @@
+package com.olegis.p0771_tabintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+}
